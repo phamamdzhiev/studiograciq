d9051b5ba18f69601a293f9215a7f134871c878e
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-07-10 03:44:49 +0300

    Change page title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Студио Грация - Дамяна Стайкова') }}</title>
+    <title>Студио Грация | Дамяна Стайкова</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
